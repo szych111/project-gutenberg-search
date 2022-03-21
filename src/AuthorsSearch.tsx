0e9ae277d0878affff86cf03dd3392098ec172ac
Alex <67001075+szych111@users.noreply.github.com>
2022-03-21 18:49:07 +0100

    authorslist

--- src/AuthorsSearch.tsx
+++ src/AuthorsSearch.tsx
@@ -2,56 +2,77 @@
 
 import { FC, useState } from "react";
 
+import { AuthorsList } from "./AuthorsList.tsx";
 import "./index.css";
 
 export const AuthorsSearch: FC = () => {
-const [userInput, setUserInput] = useState<{
-    firstName: string,
-    lastName: string,
-    date: string
-}>({
-    firstName: '',
-    lastName: '',
-    date: ''
-})
-  
-  const firstNameChangeHandler = e => {
-    setUserInput((prevUserInput) => 
-    {return {...prevUserInput, firstName: e.target.value}})
+  const [userInput, setUserInput] = useState<{
+    firstName: string;
+    lastName: string;
+  }>({
+    firstName: "",
+    lastName: "",
+  });
+
+  const firstNameChangeHandler = (e) => {
+    setUserInput((prevUserInput) => {
+      return { ...prevUserInput, firstName: e.target.value };
+    });
+  };
+  const lastNameChangeHandler = (e) => {
+    setUserInput((prevUserInput) => {
+      return { ...prevUserInput, lastName: e.target.value };
+    });
+  };
+
+  const author = {
+    name: '',
+    dateOfBirth: '',
+    dateofDeath: '',
+    lang: '',
+    books: {}
   }
-  const lastNameChangeHandler = e => {
-    setUserInput((prevUserInput) => 
-    {return {...prevUserInput, lastName: e.target.value}})
-  }
-  const dateChangeHandler = e => {
-    setUserInput((prevUserInput) => 
-    {return {...prevUserInput, date: e.target.value}})
+
+  async function fetchAuthors(name: string) {
+    const response = await fetch(`https://gutendex.com/books?search=${name}`)
+    const data = response.json();
+    console.log(data);
   }
 
   const submitHandler = (e) => {
-    e.preventDefault()
-    console.log(userInput)
-    async function fetchAuthors () {
-      const response = await fetch('https://api.artic.edu/api/v1/artworks/search?q=cats')
-      const data = response.json()
-      console.log(data)
-  }
+    e.preventDefault();
+
+    fetchAuthors(userInput.lastName);
 
     setUserInput({
-      firstName: '',
-      lastName: '', 
-      date: ''
-    })
-    console.log(userInput)
-  }
-  
+      firstName: "",
+      lastName: "",
+    });
+  };
 
   return (
-    <form className="card" onSubmit={submitHandler}>
-      <input type="text" value={userInput.firstName} placeholder="first name" onChange={firstNameChangeHandler} />
-      <input type="text" value={userInput.lastName} placeholder="last name" onChange={lastNameChangeHandler} />
-      <input type="date" value={userInput.date} placeholder="date" min="1500-01-01" onChange={dateChangeHandler} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form className="card" onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={userInput.firstName}
+          placeholder="first name"
+          onChange={firstNameChangeHandler}
+        />
+        <input
+          type="text"
+          value={userInput.lastName}
+          placeholder="last name"
+          onChange={lastNameChangeHandler}
+        />
+        <button
+          className="uppercase ff-sans-normal text-clr-dark letter-spacing-2"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+      {data && <AuthorsList />}
+    </div>
   );
 };
